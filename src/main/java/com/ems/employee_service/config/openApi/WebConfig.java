56b5923b08e10b5dcd51614e0299e1b9c747edba
bashir <bashirabdi1994@gmail.com>
2024-01-01 10:16:48 +0300

    adding content negotiation api versioning

---
+++ src/main/java/com/ems/employee_service/config/openApi/WebConfig.java
@@ -0,0 +1,16 @@
+package com.ems.employee_service.config.openApi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
+        configurer.mediaType("v1", MediaType.valueOf("application/vnd.cbc.pp-v1+json"));
+        configurer.mediaType("v2", MediaType.valueOf("application/vnd.cbc.app-v2+json"));
+    }
+}
